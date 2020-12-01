2c655baeaa6ba94339806df2d610528781f179a0
Dominic Ng <dominic.ng@conspect.nl>
2020-12-01 15:26:59 +0100

    nu met bestanden

---
+++ src/main/java/nl/conspect/drivedok/DrivedokApplication.java
@@ -0,0 +1,13 @@
+package nl.conspect.drivedok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrivedokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrivedokApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/conspect/drivedok/DrivedokApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.conspect.drivedok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrivedokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
