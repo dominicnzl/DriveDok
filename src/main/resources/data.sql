5f75bc125431e2c1fbb8642ec4d201d8056845ad
Dominic Ng <dominic.ng@conspect.nl>
2020-12-07 16:26:03 +0100

    Added some default Parking Zones in data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,8 @@
 insert into DRIVE_DOK_USER (id, name, email, password)
 values (1, 'Henk', 'abc@xyz.nl', 'pw'),
        (2, 'Ingrid', 'bcd@jkl.nl', 'pw2');
+
+insert into PARKING_ZONE (id, name, version) values (1, 'Noord', NOW());
+insert into PARKING_ZONE (id, name, version) values (2, 'Oost', NOW());
+insert into PARKING_ZONE (id, name, version) values (3, 'Zuid', NOW());
+insert into PARKING_ZONE (id, name, version) values (4, 'West', NOW());
