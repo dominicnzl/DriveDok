a6f5a593fdf97956f723f9b3a8a1179f873013a2
Dominic Ng <dominic.ng@conspect.nl>
2020-12-08 11:16:23 +0100

    Added thymeleaf template to map findall

--- src/main/java/nl/conspect/drivedok/controllers/ParkingZoneThController.java
+++ src/main/java/nl/conspect/drivedok/controllers/ParkingZoneThController.java
@@ -1,7 +1,9 @@
 package nl.conspect.drivedok.controllers;
 
 
+import nl.conspect.drivedok.services.ParkingZoneService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,14 +11,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/th")
 public class ParkingZoneThController {
 
+    private final ParkingZoneService parkingZoneService;
+
+    public ParkingZoneThController(ParkingZoneService parkingZoneService) {
+        this.parkingZoneService = parkingZoneService;
+    }
+
     @GetMapping("/")
     public String helloWorld(){
         return "helloWorld";
     }
 
-    @GetMapping("/findAll")
-    public String findAllParkingZones(){
-        return "";
+    @GetMapping("/findall")
+    public String findAllParkingZones(Model model){
+        model.addAttribute("parkingzones", parkingZoneService.findAll());
+        return "all-parkingzones";
     }
 
 }
