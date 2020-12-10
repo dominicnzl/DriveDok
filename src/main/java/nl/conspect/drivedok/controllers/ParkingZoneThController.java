0014666e1ebde7589875635cb7ef4e58045523e9
Dominic Ng <dominic.ng@conspect.nl>
2020-12-10 11:52:54 +0100

    removed "/th" from requestmapping to thymeleaf templates

--- src/main/java/nl/conspect/drivedok/controllers/ParkingZoneThController.java
+++ src/main/java/nl/conspect/drivedok/controllers/ParkingZoneThController.java
@@ -6,10 +6,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/th")
 public class ParkingZoneThController {
 
     private final ParkingZoneService parkingZoneService;
