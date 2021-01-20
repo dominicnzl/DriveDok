01761d962edc03173ebecb730b1833df3b739adc
dng <dominic.ng@conspect.nl>
2021-01-20 17:39:04 +0100

    Added static imports for ResponseEntity statuses. Changed PostMapping to return HttpStatus.CREATED

--- src/main/java/nl/conspect/drivedok/controllers/UserRestController.java
+++ src/main/java/nl/conspect/drivedok/controllers/UserRestController.java
@@ -12,10 +12,17 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+
+import static org.springframework.http.ResponseEntity.created;
+import static org.springframework.http.ResponseEntity.noContent;
+import static org.springframework.http.ResponseEntity.notFound;
+import static org.springframework.http.ResponseEntity.of;
+import static org.springframework.http.ResponseEntity.ok;
 
 @RestController
 @RequestMapping("/api/users")
@@ -30,17 +37,19 @@ public class UserRestController {
     @GetMapping
     public ResponseEntity<UserList> findAllUsers() {
         var userList = new UserList(userService.findAll());
-        return ResponseEntity.ok(userList);
+        return ok(userList);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<User> findById(@PathVariable Long id) {
-        return ResponseEntity.of(userService.findById(id));
+        return of(userService.findById(id));
     }
 
     @PostMapping
-    public ResponseEntity<User> create(@RequestBody User user) {
-        return ResponseEntity.ok(userService.save(user));
+    public ResponseEntity<User> create(@RequestBody User user, UriComponentsBuilder builder) {
+        var createdUser = userService.save(user);
+        var uri = builder.path("/api/users/{id}").buildAndExpand(createdUser.getId()).toUri();
+        return created(uri).body(createdUser);
     }
 
     @PutMapping
@@ -50,23 +59,23 @@ public class UserRestController {
                 .flatMap(userService::findById)
                 .map(u -> userService.update(u.getId(), user))
                 .map(ResponseEntity::ok)
-                .orElseGet(() -> ResponseEntity.notFound().build());
+                .orElseGet(() -> notFound().build());
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User user) {
-        return ResponseEntity.ok(userService.update(id, user));
+        return ok(userService.update(id, user));
     }
 
     @PatchMapping("/{id}")
     public ResponseEntity<User> partiallyUpdate(@PathVariable Long id, @RequestBody Map<String, String> properties) {
-        return ResponseEntity.ok(userService.updatePartially(id, properties));
+        return ok(userService.updatePartially(id, properties));
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
         userService.deleteById(id);
-        return ResponseEntity.noContent().build();
+        return noContent().build();
     }
 
     static class UserList {
@@ -76,7 +85,7 @@ public class UserRestController {
             this.users = users;
         }
 
-        public List<User> getUsers() {
+        public final List<User> getUsers() {
             return users;
         }
     }
