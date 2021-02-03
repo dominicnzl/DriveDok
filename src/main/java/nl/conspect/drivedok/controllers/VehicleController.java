e3faf2eca884262b5b482d91907dc3a045656e4c
dng <dominic.ng@conspect.nl>
2021-02-03 11:03:33 +0100

    Closes #88: Cleaned up false comment in UserController. Removed unused post in VehicleController. Cleaned up doubled link in VehicleEditPage.

--- src/main/java/nl/conspect/drivedok/controllers/VehicleController.java
+++ src/main/java/nl/conspect/drivedok/controllers/VehicleController.java
@@ -9,7 +9,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Objects;
@@ -31,12 +30,6 @@ public class VehicleController {
     public String listpage(Model model) {
         model.addAttribute("vehicles", vehicleService.findAll());
         return "vehiclelistpage";
-    }
-
-    @PostMapping
-    public String createEditpage(Model model) {
-        model.addAttribute("vehicle", new Vehicle());
-        return "vehicleeditpage";
     }
 
     @GetMapping("/{id}")
