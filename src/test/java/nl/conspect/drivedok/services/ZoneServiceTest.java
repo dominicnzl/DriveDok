e55f98646e0cc34bd616a3d06be0847bd0251a95
Eva van Geenen <evavangeenen@gmail.com>
2021-03-03 16:08:53 +0100

    Tests for ZoneService

--- src/test/java/nl/conspect/drivedok/services/ZoneServiceTest.java
+++ src/test/java/nl/conspect/drivedok/services/ZoneServiceTest.java
@@ -1,137 +1,106 @@
 package nl.conspect.drivedok.services;
 
+import nl.conspect.drivedok.exceptions.ParkingSpotUpdateException;
 import nl.conspect.drivedok.model.ParkingSpot;
 import nl.conspect.drivedok.model.ParkingType;
 import nl.conspect.drivedok.model.Zone;
 import nl.conspect.drivedok.repositories.ZoneRepository;
-import org.junit.jupiter.api.BeforeEach;
+import nl.conspect.drivedok.utilities.ParkingTypeComparator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.transaction.annotation.Transactional;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.function.Executable;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
 
-import java.util.Iterator;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.stream.Stream;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
-@DataJpaTest
-@Transactional
+@ExtendWith(MockitoExtension.class)
 class ZoneServiceTest {
 
-    ZoneService zoneService;
+    @Spy
+    @InjectMocks
+    BasicZoneService zoneService;
 
-    @Autowired
-    TestEntityManager testEntityManager;
-
-    @Autowired
+    @Mock
     ZoneRepository zoneRepository;
 
-    Zone zone1;
-    Zone zone2;
+    @Spy
+    ParkingSpot parkingSpot;
 
-    @BeforeEach
-    public void init() {
-        zoneService = new ZoneServiceImpl(zoneRepository);
-        zone1 = new Zone("Elsa",100);
-        zone2 = new Zone("Anna",200);
-        zoneService.create(zone1);
-        zoneService.create(zone2);
-        testEntityManager.persist(zone1);
-        testEntityManager.persist(zone2);
+    @Test
+    @DisplayName("Assert that all Zones are returned.")
+    void shouldFindAllZones(){
+        Zone zone1 = new Zone("Noord", 100);
+        Zone zone2 = new Zone("Oost", 200);
+        Zone zone3 = new Zone("Zuid", 300);
+        List<Zone> expected = new ArrayList<Zone>(Arrays.asList(zone1, zone2, zone3));
+
+        when(zoneRepository.findAll()).thenReturn(expected);
+        List<Zone> actual = zoneService.findAll();
+
+        assertEquals(expected, actual);
     }
 
     @Test
-    @DisplayName("Expect findAll() to return a list with size 2")
-    void findAll() {
-        assertEquals(2, zoneService.findAll().size());
+    @DisplayName("Expect the right Zone to be returned.")
+    void shouldFindZoneById(){
+        Zone zone1 = new Zone("Noord", 100);
+        when(zoneRepository.findById(any())).thenReturn(Optional.of(zone1));
+
+        Zone actual = zoneService.findById(1L).get();
+        assertEquals(zone1, actual);
     }
 
     @Test
-    @DisplayName("Expect findById to retrieve the correct Zone. Verify by checking the name")
-    void findById() {
-        Zone zone2 = new Zone("Anna",300);
-        var id = testEntityManager.persistAndGetId(zone2, Long.class);
-        String name = zoneService.findById(id)
-                .map(Zone::getName)
-                .orElse(null);
-        assertEquals("Anna", name);
+    void shouldCreateZone(){
+        Zone zone1 = new Zone("Noord", 100);
+        when(zoneRepository.save(zone1)).thenReturn(zone1);
+        Zone actual = zoneService.create(zone1);
+        assertEquals(zone1, actual);
     }
 
     @Test
-    @DisplayName("Assert size of list is 2. Create a new zone and expect list to be size 3")
-    void create() {
-        assertEquals(2, zoneService.findAll().size());
-        Zone zone = new Zone("Elsa", 100);
-        zoneService.create(zone);
-        assertEquals(3, zoneService.findAll().size());
-
+    void shouldReturnSameZone_When_Updated(){
+        Zone zone1 = new Zone("Noord", 100);
+        when(zoneRepository.save(zone1)).thenReturn(zone1);
+        Zone updatedZone = zoneService.update(zone1);
+        assertSame(zone1, updatedZone);
     }
 
     @Test
-    @DisplayName("The Zone should be correctly updated")
-    void updateZone() {
-        Zone beforeUpdateZone = zoneService.create(new Zone("Noord",100));
-        assertNotNull(beforeUpdateZone);
-        Iterator<ParkingSpot> iterator = beforeUpdateZone.getParkingSpots().iterator();
-        ParkingSpot NORMALspot = iterator.next();
-        ParkingSpot DISABLEDspot = iterator.next();
-        ParkingSpot ELECTRICspot = iterator.next();
-        ParkingSpot MOTORspot = iterator.next();
+    void shouldThrowException_When_TotalParkingSpotsIsNotSumOfParkingSpotsQuantity(){
+        Zone zone = new Zone();
+        zone.setTotalParkingSpots(100);
+        Set<ParkingSpot> list = new TreeSet<>(new ParkingTypeComparator());
+        ParkingSpot ps = new ParkingSpot();
+        ps.setQuantity(50);
+        ps.setParkingType(ParkingType.NORMAL);
+        list.add(ps);
+        zone.setParkingSpots(list);
 
-        assertEquals(100, NORMALspot.getQuantity());
-        assertEquals(0, DISABLEDspot.getQuantity());
-        assertEquals(0, ELECTRICspot.getQuantity());
-        assertEquals(0, MOTORspot.getQuantity());
+        Executable executable = () -> zoneService.update(zone);
 
-        NORMALspot.setQuantity(80);
-        DISABLEDspot.setQuantity(10);
-        ELECTRICspot.setQuantity(5);
-        MOTORspot.setQuantity(5);
-
-        Zone afterUpdateZone = zoneService.update(beforeUpdateZone);
-        assertNotNull(afterUpdateZone);
-        Iterator<ParkingSpot> iterator2 = afterUpdateZone.getParkingSpots().iterator();
-
-        ParkingSpot NORMALspot2 = iterator2.next();
-        ParkingSpot DISABLEDspot2 = iterator2.next();
-        ParkingSpot ELECTRICspot2 = iterator2.next();
-        ParkingSpot MOTORspot2 = iterator2.next();
-
-        assertEquals(80, NORMALspot2.getQuantity());
-        assertEquals(10, DISABLEDspot2.getQuantity());
-        assertEquals(5, ELECTRICspot2.getQuantity());
-        assertEquals(5, MOTORspot2.getQuantity());
+        assertThrows(ParkingSpotUpdateException.class, executable);
     }
 
-
-    @Test
-    @DisplayName("Delete a zone. Expect findAll() to have size 1")
-    void deleteById() {
-        Zone zone = new Zone("Elsa", 100);
-        Zone zone1 = zoneService.create(zone);
-        assertEquals(3, zoneService.findAll().size());
-        zoneService.deleteById(zone1.getId());
-        testEntityManager.flush();
-        testEntityManager.clear();
-        assertEquals(2, zoneService.findAll().size());
-    }
-
-    @Test
-    @DisplayName("Create zone and expect first parkingspot of type NORMAL and quantity equal to totalParkingSpots")
-    void createAndCheckNormalParkingSpots(){
-        Zone zone1 = zoneService.create(new Zone("Zone 1", 500));
-        assertEquals(ParkingType.NORMAL, zone1.getParkingSpots().iterator().next().getParkingType());
-        assertEquals(zone1.getTotalParkingSpots(), zone1.getParkingSpots().iterator().next().getQuantity());
-    }
-
-    @Test
-    @DisplayName("FindAll zones and assert their first parkingspottype is NORMAL")
-    void findAllAndAssertFirstPSTypeIsNORMAL(){
-        List<Zone> all = zoneService.findAll();
-         all.forEach(zone -> assertEquals(ParkingType.NORMAL, zone.getParkingSpots().iterator().next().getParkingType()));
-    }
 }
