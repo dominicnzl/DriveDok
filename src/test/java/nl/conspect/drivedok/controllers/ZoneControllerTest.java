a42641133f3762a9ecdf891684850e4f79c461bb
Eva van Geenen <evavangeenen@gmail.com>
2021-03-05 09:35:43 +0100

    Some modifications on tests for the zone controller

--- src/test/java/nl/conspect/drivedok/controllers/ZoneControllerTest.java
+++ src/test/java/nl/conspect/drivedok/controllers/ZoneControllerTest.java
@@ -11,17 +11,21 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.hamcrest.Matchers.containsString;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 @WebMvcTest(ZoneController.class)
 class ZoneControllerTest {
@@ -33,6 +37,21 @@ class ZoneControllerTest {
     ZoneService zoneService;
 
     @Test
+    void should_Show_ZoneListPage() throws Exception {
+        Zone zone1 = new Zone("Noord", 100);
+        Zone zone2 = new Zone("Oost", 200);
+        Zone zone3 = new Zone("Zuid", 300);
+        List<Zone> expected = new ArrayList<Zone>(Arrays.asList(zone1, zone2, zone3));
+        when(zoneService.findAll()).thenReturn(expected);
+
+        mockMvc.perform(get("/zones"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("zones"))
+                .andReturn();
+    }
+
+    @Test
     public void homeShouldShowThereAreNoZonesYet() throws Exception {
         mockMvc.perform(get("/zones"))
                 .andDo(print())
@@ -42,7 +61,7 @@ class ZoneControllerTest {
     }
 
     @Test
-    public void shouldShowListOfZones() throws Exception {
+    public void shouldContainListOfZones() throws Exception {
         List<Zone> listPZ = new ArrayList<>();
         Zone pz1 = new Zone("Zone 1", 100);
         Zone pz2 = new Zone("Zone 2",200);
@@ -93,15 +112,16 @@ class ZoneControllerTest {
 
 
     @Test
-    public void shouldDeleteZone() throws Exception {
+    public void shouldGoToZoneListPageAfterDeletingZone() throws Exception {
 
         Zone pz1 = new Zone("Zone 1", 100);
         when(zoneService.findById(1L))
                 .thenReturn(Optional.of(pz1));
 
         mockMvc.perform(MockMvcRequestBuilders.get("/zones/delete/{id}", 1))
-                .andExpect(status().isOk());
-
+                .andExpect(status().isOk())
+                .andExpect(view().name("zonelistpage"))
+        ;
     }
 
     @Test
