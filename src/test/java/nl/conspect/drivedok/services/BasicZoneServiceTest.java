ae01e5d0f146040d14e00e7729c8266427a1f775
Eva van Geenen <evavangeenen@gmail.com>
2021-03-05 09:34:06 +0100

    Changed name of tests for the zoneservice

--- src/test/java/nl/conspect/drivedok/services/BasicZoneServiceTest.java
+++ src/test/java/nl/conspect/drivedok/services/BasicZoneServiceTest.java
@@ -35,17 +35,13 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-class ZoneServiceTest {
+class BasicZoneServiceTest {
 
-    @Spy
     @InjectMocks
     BasicZoneService zoneService;
 
     @Mock
     ZoneRepository zoneRepository;
-
-    @Spy
-    ParkingSpot parkingSpot;
 
     @Test
     @DisplayName("Assert that all Zones are returned.")
@@ -54,49 +50,57 @@ class ZoneServiceTest {
         Zone zone2 = new Zone("Oost", 200);
         Zone zone3 = new Zone("Zuid", 300);
         List<Zone> expected = new ArrayList<Zone>(Arrays.asList(zone1, zone2, zone3));
-
         when(zoneRepository.findAll()).thenReturn(expected);
+
         List<Zone> actual = zoneService.findAll();
 
         assertEquals(expected, actual);
     }
 
     @Test
-    @DisplayName("Expect the right Zone to be returned.")
+    @DisplayName("Expect the right Zone to be returned when finding by Id.")
     void shouldFindZoneById(){
         Zone zone1 = new Zone("Noord", 100);
         when(zoneRepository.findById(any())).thenReturn(Optional.of(zone1));
 
         Zone actual = zoneService.findById(1L).get();
-        assertEquals(zone1, actual);
+
+        assertSame(zone1, actual);
     }
 
     @Test
-    void shouldCreateZone(){
+    @DisplayName("Expect the same Zone to be returned when creating the zone.")
+    void shouldReturnSameZone_When_CreateZone(){
         Zone zone1 = new Zone("Noord", 100);
         when(zoneRepository.save(zone1)).thenReturn(zone1);
+
         Zone actual = zoneService.create(zone1);
-        assertEquals(zone1, actual);
+
+        assertSame(zone1, actual);
     }
 
     @Test
+    @DisplayName("Expect the same Zone to be returned when the zone is updated.")
     void shouldReturnSameZone_When_Updated(){
         Zone zone1 = new Zone("Noord", 100);
         when(zoneRepository.save(zone1)).thenReturn(zone1);
+
         Zone updatedZone = zoneService.update(zone1);
+
         assertSame(zone1, updatedZone);
     }
 
     @Test
+    @DisplayName("Expect a ParkingSpotUpdateException when totalParkingSpots is not the same as sum of parkingspots.")
     void shouldThrowException_When_TotalParkingSpotsIsNotSumOfParkingSpotsQuantity(){
         Zone zone = new Zone();
         zone.setTotalParkingSpots(100);
-        Set<ParkingSpot> list = new TreeSet<>(new ParkingTypeComparator());
-        ParkingSpot ps = new ParkingSpot();
-        ps.setQuantity(50);
-        ps.setParkingType(ParkingType.NORMAL);
-        list.add(ps);
-        zone.setParkingSpots(list);
+        Set<ParkingSpot> parkingSpotSet = new TreeSet<>(new ParkingTypeComparator());
+        ParkingSpot parkingSpot = new ParkingSpot();
+        parkingSpot.setQuantity(50);
+        parkingSpot.setParkingType(ParkingType.NORMAL);
+        parkingSpotSet.add(parkingSpot);
+        zone.setParkingSpots(parkingSpotSet);
 
         Executable executable = () -> zoneService.update(zone);
 
